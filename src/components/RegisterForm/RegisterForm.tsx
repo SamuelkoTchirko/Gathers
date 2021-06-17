f6826ac713bf7ea4dfff21149ddc47fdc2aa35cd
Samuel Gabriel Tchir <samuelgabrieltchir@gmail.com>
2021-06-17 20:53:11 +0200

    Register Form Done

--- src/components/RegisterForm/RegisterForm.tsx
+++ src/components/RegisterForm/RegisterForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider } from '@ionic/react';
+import { IonButton, IonGrid, IonCol, IonRow, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonItem, IonLabel, IonList, IonItemDivider } from '@ionic/react';
 import styles from './RegisterForm.module.scss';
 
 const RegisterForm: React.FC = () => {
@@ -10,10 +10,62 @@ const RegisterForm: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-          <IonItem className={styles.nickname}>
-            <IonLabel position="fixed">Register</IonLabel>
-            <IonInput value="Epic"></IonInput>
-          </IonItem>
+        <IonGrid>
+          <IonRow className={styles.upperPadding}>
+            
+          </IonRow>
+          <IonRow>
+            <IonCol>
+
+            </IonCol>
+            <IonCol size="10">
+              <IonItem>
+                <IonLabel position="floating">Pouzivatelske Meno</IonLabel>
+                <IonInput value={text}></IonInput>
+              </IonItem>
+            </IonCol>
+            <IonCol>
+
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+
+            </IonCol>
+            <IonCol size="10">
+              <IonItem>
+                <IonLabel position="floating">Heslo</IonLabel>
+                <IonInput type="password" value={text}></IonInput>
+              </IonItem>
+            </IonCol>
+            <IonCol>
+
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+
+            </IonCol>
+            <IonCol size="10">
+              <IonItem>
+                <IonLabel position="floating">Zopakuj Heslo</IonLabel>
+                <IonInput type="password" value={text}></IonInput>
+              </IonItem>
+            </IonCol>
+            <IonCol>
+
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol></IonCol>
+            <IonCol size="4">
+              <br />
+              <br />
+              <IonButton expand="block" fill="outline">Zaregistruj Sa</IonButton>
+            </IonCol>
+            <IonCol></IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
